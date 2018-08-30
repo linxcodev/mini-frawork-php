ae5a776dfa668fa936a6afe16e00f8857958050b
Ahmad Fajrul Falah <fajrul452@gmail.com>
2018-08-30 20:55:03 +0700

    menghubungkan view dengan asset css

--- app/Views/home.php
+++ app/Views/home.php
@@ -3,9 +3,12 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <link rel="stylesheet" href="../../public/css/main.css">
   </head>
   <body>
-    ini view dari home milik mas <b><?= $data['nama'] ?></b>
-    yang berumur <b><?= $data['umur'] ?></b>
+    <div class="">
+      ini view dari home milik mas <b><?= $data['nama'] ?></b>
+      yang berumur <b><?= $data['umur'] ?></b>
+    </div>
   </body>
 </html>
